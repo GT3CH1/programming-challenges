c74ee14275620b01eb5b70a0bb5eb9a7740330cb
Gavin Pease <gavinpease@gmail.com>
2023-05-12 12:02:48 -0600

    More tests.

--- src/com/peasenet/tests/ParenthesisAlgorithmTest.java
+++ src/com/peasenet/tests/ParenthesisAlgorithmTest.java
@@ -3,6 +3,7 @@ package com.peasenet.tests;
 import com.peasenet.parenthesis.ParenthesisAlgorithm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
 
 /**
  * @author gt3ch1
@@ -42,5 +43,48 @@ public class ParenthesisAlgorithmTest {
         var str = "([)]";
         var res = ParenthesisAlgorithm.isValid(str);
         Assertions.assertFalse(res, "Expected false but got " + res);
+    }
+
+    @Test
+    void simple_bad_4() {
+        var str = "{{]}";
+        var res = ParenthesisAlgorithm.isValid(str);
+        Assertions.assertFalse(res, "Expected false but got " + res);
+    }
+
+    @Test
+    void simple_bad_5() {
+        var str = "({)}";
+        var res = ParenthesisAlgorithm.isValid(str);
+        Assertions.assertFalse(res, "Expected false but got " + res);
+    }
+
+    @Test
+    void complex_good_1() {
+        var str = "([{([{}])}])";
+        var res = ParenthesisAlgorithm.isValid(str);
+        Assertions.assertTrue(res, "Expected true but got " + res);
+    }
+
+    @Test
+    void simple_good() {
+        var str = "(){}[]";
+        var res = ParenthesisAlgorithm.isValid(str);
+        Assertions.assertTrue(res, "Expected true but got " + res);
+    }
+
+    @Test
+    void edge_case() {
+        var res = ParenthesisAlgorithm.isValid("(");
+        Assertions.assertFalse(res, "Expected true but got " + res);
+        res = ParenthesisAlgorithm.isValid("[");
+        Assertions.assertFalse(res, "Expected true but got " + res);
+        res = ParenthesisAlgorithm.isValid("{");
+        Assertions.assertFalse(res, "Expected true but got " + res);
+        res = ParenthesisAlgorithm.isValid("(}");
+        Assertions.assertFalse(res, "Expected false but got " + res);
+        res = ParenthesisAlgorithm.isValid("]");
+        Assertions.assertFalse(res, "Expected false but got " + res);
+        res = ParenthesisAlgorithm.isValid("[()");
     }
 }
